23a2e852525989218518a84b539287a849b06f32
jooda00 <dragon2000a@gmail.com>
2024-08-01 14:13:23 +0900

    feat: setting

---
+++ src/main/java/com/wanted/pre/onboarding/backend/PreOnboardingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.pre.onboarding.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnboardingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreOnboardingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/pre/onboarding/backend/PreOnboardingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.pre.onboarding.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnboardingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
