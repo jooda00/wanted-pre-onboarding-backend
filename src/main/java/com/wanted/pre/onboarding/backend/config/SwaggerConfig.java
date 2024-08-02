aa0d0d3865a61762a1ac6f1d9aa0b0fbb65f1c0c
jooda00 <dragon2000a@gmail.com>
2024-08-02 14:10:03 +0900

    chore: swagger config 작성

---
+++ src/main/java/com/wanted/pre/onboarding/backend/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.wanted.pre.onboarding.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.components(new Components())
+			.info(apiInfo());
+	}
+
+	private Info apiInfo() {
+		return new Info()
+			.title("Spring Boot REST API Specifications")
+			.description("원티드 프리온보딩 백엔드 과제")
+			.version("1.0.0");
+	}
+}
+
