301cb71081ede9345f2659cdf98254e147cefa9c
jooda00 <dragon2000a@gmail.com>
2024-08-02 17:56:14 +0900

    test: 채용공고 수정 및 예외처리 service 메소드 테스트
    -updateRecruitment(), updateRecruitmentWithNonExistentRecruitment()

--- src/test/java/com/wanted/pre/onboarding/backend/service/recruitment/RecruitmentServiceTest.java
+++ src/test/java/com/wanted/pre/onboarding/backend/service/recruitment/RecruitmentServiceTest.java
@@ -1,6 +1,7 @@
 package com.wanted.pre.onboarding.backend.service.recruitment;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -15,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.wanted.pre.onboarding.backend.dto.recruitment.RecruitmentRequest;
+import com.wanted.pre.onboarding.backend.dto.recruitment.RecruitmentUpdate;
 import com.wanted.pre.onboarding.backend.entity.company.Company;
 import com.wanted.pre.onboarding.backend.entity.recruitment.Recruitment;
 import com.wanted.pre.onboarding.backend.repository.company.CompanyRepository;
@@ -33,10 +35,13 @@ class RecruitmentServiceTest {
 	private RecruitmentService recruitmentService;
 
 	private Company company;
+	private Recruitment targetRecruitment;
 
 	@BeforeEach
 	void setUp() {
 		company = new Company(1L, "원티드", "한국", "서울");
+		targetRecruitment = new Recruitment(1L, "백엔드 주니어 개발자", 1000, "자프링 개발자 모집합니다.", "Java");
+		targetRecruitment.setCompany(company);
 	}
 
 	@Test
@@ -72,5 +77,43 @@ class RecruitmentServiceTest {
 
 		verify(companyRepository).findById(nonExistentCompanyId);
 		verify(recruitmentRepository, never()).save(any(Recruitment.class));
+	}
+
+	@Test
+	@DisplayName("채용공고를 수정하면 정상적으로 수정된다.")
+	void updateRecruitment() {
+		// Given
+		RecruitmentUpdate update = new RecruitmentUpdate("백엔드 시니어 개발자", 3000000, "10년 이상 백엔드 개발자 모집합니다.", "Java");
+
+		when(companyRepository.findById(company.getId())).thenReturn(Optional.of(company));
+		when(recruitmentRepository.findById(targetRecruitment.getId())).thenReturn(Optional.of(targetRecruitment));
+
+		// When
+		recruitmentService.updateRecruitment(company.getId(), targetRecruitment.getId(), update);
+
+		// Then
+		verify(companyRepository).findById(company.getId());
+		verify(recruitmentRepository).findById(targetRecruitment.getId());
+
+		// 변경 사항 검증
+		assertEquals("백엔드 시니어 개발자", targetRecruitment.getPosition());
+		assertEquals(3000000, targetRecruitment.getCompensation());
+		assertEquals("10년 이상 백엔드 개발자 모집합니다.", targetRecruitment.getContent());
+	}
+
+	@Test
+	@DisplayName("존재하지 않는 채용공고를 수정하면 예외가 발생한다.")
+	void updateRecruitmentWithNonExistentRecruitment() {
+		// Given
+		Long nonExistentRecruitmentId = 2L;
+		RecruitmentUpdate update = new RecruitmentUpdate("백엔드 시니어 개발자", 3000000, "10년 이상 백엔드 개발자 모집합니다.", "Java");
+
+		when(companyRepository.findById(company.getId())).thenReturn(Optional.of(company));
+		when(recruitmentRepository.findById(nonExistentRecruitmentId)).thenReturn(Optional.empty());
+
+		// When & Than
+		assertThatThrownBy(() -> recruitmentService.updateRecruitment(company.getId(), nonExistentRecruitmentId, update))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("해당 채용공고는 존재하지 않습니다.");
 	}
 }
