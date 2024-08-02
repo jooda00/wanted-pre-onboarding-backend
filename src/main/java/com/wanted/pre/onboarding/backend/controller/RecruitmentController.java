e08039ed77f29a84c60418d8d76ba6ba50803618
jooda00 <dragon2000a@gmail.com>
2024-08-02 17:53:56 +0900

    feat: 채용공고 수정 controller 메소드 작성

--- src/main/java/com/wanted/pre/onboarding/backend/controller/RecruitmentController.java
+++ src/main/java/com/wanted/pre/onboarding/backend/controller/RecruitmentController.java
@@ -2,12 +2,15 @@ package com.wanted.pre.onboarding.backend.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wanted.pre.onboarding.backend.dto.common.CommonResponse;
 import com.wanted.pre.onboarding.backend.dto.recruitment.RecruitmentRequest;
+import com.wanted.pre.onboarding.backend.dto.recruitment.RecruitmentUpdate;
 import com.wanted.pre.onboarding.backend.service.recruitment.RecruitmentService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +24,14 @@ public class RecruitmentController {
 	public ResponseEntity<CommonResponse> createRecruitment(@RequestBody RecruitmentRequest request) {
 		recruitmentService.saveRecruitment(request);
 		CommonResponse response = new CommonResponse(HttpStatus.CREATED, request);
+		return new ResponseEntity<>(response, response.getCode());
+	}
+
+	@PutMapping("/v1/recruitments/{companyId}/{recruitmentId}")
+	public ResponseEntity<CommonResponse> updateRecruitment(@PathVariable("companyId") Long companyId, @PathVariable("recruitmentId") Long recruitmentId,
+		@RequestBody RecruitmentUpdate update) {
+		recruitmentService.updateRecruitment(companyId, recruitmentId, update);
+		CommonResponse response = new CommonResponse(HttpStatus.CREATED, update);
 		return new ResponseEntity<>(response, response.getCode());
 	}
 }
