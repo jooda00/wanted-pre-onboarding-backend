04fa42a9bc750265c832a726d456a2f672f758d0
jooda00 <dragon2000a@gmail.com>
2024-08-05 15:06:17 +0900

    feat: 채용공고, 사용자와 각각 1:N 매핑

--- src/main/java/com/wanted/pre/onboarding/backend/entity/user/User.java
+++ src/main/java/com/wanted/pre/onboarding/backend/entity/user/User.java
@@ -1,9 +1,16 @@
 package com.wanted.pre.onboarding.backend.entity.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wanted.pre.onboarding.backend.entity.application.Application;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -12,4 +19,7 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+	private List<Application> applications = new ArrayList<>();
 }
