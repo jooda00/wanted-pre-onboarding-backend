d3ae4144a789faf79486109823c71bd6881f119d
jooda00 <dragon2000a@gmail.com>
2024-08-09 00:16:40 +0900

    test: 회사에 속하지 않은 채용공고 수정 service 메소드 예외 테스트
    - updateRecruitmentWithNonRelatedCompany()

--- src/test/java/com/wanted/pre/onboarding/backend/service/recruitment/RecruitmentServiceExceptionTest.java
+++ src/test/java/com/wanted/pre/onboarding/backend/service/recruitment/RecruitmentServiceExceptionTest.java
@@ -3,6 +3,7 @@ package com.wanted.pre.onboarding.backend.service.recruitment;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Collections;
 import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.wanted.pre.onboarding.backend.dto.recruitment.RecruitmentRequest;
@@ -27,6 +29,12 @@ public class RecruitmentServiceExceptionTest {
 
 	@Mock
 	private RecruitmentRepository recruitmentRepository;
+
+	@Spy
+	private Company nonRelatedCompany = new Company(3L, "비관련 회사", "한국", "부산");
+
+	@Spy
+	private Recruitment recruitment = new Recruitment(1L, "백엔드 주니어 개발자", 1000, "자프링 개발자 모집합니다.", "Java");
 
 	@InjectMocks
 	private RecruitmentService recruitmentService;
@@ -68,6 +76,26 @@ public class RecruitmentServiceExceptionTest {
 
 		verify(companyRepository).findById(company.getId());
 		verify(recruitmentRepository).findById(nonExistentRecruitmentId);
-		verify(recruitmentRepository, never()).save(any(Recruitment.class));
+	}
+
+	@Test
+	@DisplayName("해당 회사가 작성하지 않은 채용공고를 수정하면 예외가 발생한다.")
+	void updateRecruitmentWithNonRelatedCompany() {
+		// Given
+		recruitment.setCompany(new Company(2L, "원티드", "한국", "서울"));
+		RecruitmentUpdate update = new RecruitmentUpdate("백엔드 시니어 개발자", 3000000, "10년 이상 백엔드 개발자 모집합니다.", "Java");
+
+		when(companyRepository.findById(nonRelatedCompany.getId())).thenReturn(Optional.of(nonRelatedCompany));
+		when(recruitmentRepository.findById(recruitment.getId())).thenReturn(Optional.of(recruitment));
+		when(nonRelatedCompany.getRecruitments()).thenReturn(Collections.emptyList());
+
+		// When & Then
+		assertThatThrownBy(() -> recruitmentService.updateRecruitment(nonRelatedCompany.getId(), recruitment.getId(), update))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessage("귀사가 작성하지 않은 채용공고는 수정할 수 없습니다.");
+
+		verify(companyRepository).findById(nonRelatedCompany.getId());
+		verify(recruitmentRepository).findById(recruitment.getId());
+		verify(recruitment, never()).updateRecruitment(any(RecruitmentUpdate.class));
 	}
 }
